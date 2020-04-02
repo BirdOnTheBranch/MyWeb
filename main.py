16ac8586a68fa63e66c1d0dfbe6b242655ec11c9
BirdOnTheBranch <Planelles.luis@gmail.com>
2020-04-02 18:21:30 +0200

    New structure

--- main.py
+++ main.py
@@ -1,53 +1,16 @@
-from flask import Flask, request, g, redirect, url_for, render_template, flash, make_response
-from werkzeug.security import check_password_hash, generate_password_hash
-import sqlite3
+from flask import Flask, render_template
 import requests
 
 
-# configuration
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "rajsiosorqwnejrq39834tergm4"
-app.config['DATABASE'] = 'flaskr.db'
 app.config['DEBUG'] = True
 
-
-# connect to database
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-# create the database
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-# open database connection
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-# close database connection
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
 def create_app():
-    """Creates the app with cache instance and Blueprints registered"""
+    """Creates the app """
     app = Flask(__name__)
     app.config['CACHE_TYPE'] = 'simple'
 
     return app
-
 
 
 @app.route('/')
